7889827975676e60d42e91b3a3494adbbe445d33
Francesco Filicetti <francesco.filicetti@unical.it>
2023-04-11 14:39:01 +0200

    fix: style

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-unical-bootstrap-italia',
-    version='2.0.1',
+    version='2.0.2',
     packages=find_packages(),
     include_package_data=True,
     license='Apache 2.0',
